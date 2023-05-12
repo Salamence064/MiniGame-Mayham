60e4a13a89460571a66582a22e76852d189d06f0
Salamence064 <thomas.ducote64@gmail.com>
2023-05-11 23:42:29 -0500

    Raycasting vs 2D AABB

--- physics.h
+++ physics.h
@@ -3,7 +3,11 @@
 
 #include "zmath.h"
 
-namespace Primitives {
+namespace Physics {
+    // * ======================
+    // * Primitives Needed
+    // * ======================
+
     class Ray2D {
         public:
             ZMath::Vec2D origin;
@@ -50,10 +54,53 @@ namespace Primitives {
                 return v;
             };
     };
-}
 
-namespace Physics {
-    bool raycast(const Ray2D &ray, const AABB &aabb) const;
+
+    // * ==================
+    // * Raycasting
+    // * ==================
+
+    // Determine if a ray intersects an AABB.
+    // dist will be modified to equal the distance from the ray it hits the AABB.
+    // dist is set to -1 if there is no intersection.
+    bool raycast(const Ray2D &ray, const AABB &aabb, float &dist) const {
+        // ? We can determine the distance from the ray to a certain edge by dividing a select min or max vector component
+        // ?  by the corresponding component from the unit directional vector.
+        // ? We know if tMin > tMax, then we have no intersection and if tMax is negative the AABB is behind us and we do not have a hit.
+
+        ZMath::Vec2D dirFrac(1.0f/ray.dir.x, 1.0f/ray.dir.y);
+        ZMath::Vec2D min = aabb.getMin(), max = aabb.getMax();
+
+        float t1 = (min.x - ray.origin.x)*dirfrac.x;
+        float t2 = (max.x - ray.origin.x)*dirfrac.x;
+        float t3 = (min.y - ray.origin.y)*dirfrac.y;
+        float t4 = (max.y - ray.origin.y)*dirfrac.y;
+
+        // tMin is the max of the mins and tMax is the min of the maxes
+        float tMin = ZMath::max(ZMath::min(t1, t2), ZMath::min(t3, t4));
+        float tMax = ZMath::min(ZMath::max(t1, t2), ZMath::max(t3, t4));
+
+        // if tMax < 0 the ray is intersecting behind it. Therefore, we do not actually have a collision.
+        if (tMax < 0) {
+            dist = -1.0f;
+            return 0;
+        }
+
+        // ray doesn't intersect the AABB.
+        if (tMax < tMin) {
+            dist = -1.0f;
+            return 0;
+        }
+
+        // ray's origin is inside of the AABB.
+        if (tMin < 0) {
+            dist = tMax;
+            return 1;
+        }
+
+        dist = tMin;
+        return 1;
+    };
 }
 
 #endif // !PHYSICS_H
