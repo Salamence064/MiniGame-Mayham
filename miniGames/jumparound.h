f643baba961221a2c6361a6cdb06cbfebc37be80
Salamence064 <thomas.ducote64@gmail.com>
2023-05-13 17:30:11 -0500

    Jump Around update

--- miniGames/jumparound.h
+++ miniGames/jumparound.h
@@ -15,29 +15,62 @@ namespace JumpAround {
 
         // Make the player jump. Called everytime the player touches the ground.
         // todo test for an ideal initial y velocity.
-        void jump() { vel.y = 50.0f; };
+        void jump() { vel.y = 20.0f; };
     };
 
     // Scrolling stage for the jump around mini-game.
     class Stage {
         private:
+            ZMath::Vec2D origin; // initial position of the player
+
             Physics::AABB* blocks;
             uint numBlocks;
+
+            Physics::AABB* ground;
+            uint numGround;
 
             Physics::AABB goal; // The end of the platformer.
             Player player;
 
             float grav = -9.8f; // todo test for ideal gravity value
+            float xOffset = 0.0f; // Horizontal offset for the player with respect to the full map.
 
             // Restart the level. Called whenever the player dies.
-            inline void restart();
+            inline void restart() {
+                player.pos = origin;
+                player.hitbox.c = origin;
+                player.vel.zero();
+                xOffset = 0.0f;
+            };
 
         public:
             bool completed = 0; // Is the stage completed or not? 1 = yes, 0 = no. Cached for efficiency.
 
             Stage();
 
-            void update(float dt);
+            // Move the player. Left parameter determines if the player is moving left or right.
+            void move(bool left) {
+                // todo determine an appropriate speed value
+                if (left) { player.vel.x = -20.0f; }
+                else { player.vel.y = 20.0f; }
+            };
+
+            void update(float dt) {
+                // check if the player needs to jump
+                for (uint i = 0; i < numGround; ++i) {
+                    if (Physics::CircleAndAABB(player.hitbox, ground[i])) {
+                        player.jump();
+                        break;
+                    }
+                }
+
+                // todo add collision checks with walls
+                // todo add checks for falling to your death (check if the player touches 0 y level)
+
+                // update the player
+                player.vel.y += grav * dt;
+                player.pos += player.vel * dt;
+            };
 
             void draw() const;
     };
